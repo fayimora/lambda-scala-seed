26e35be6a52e947a7f04c4b6429f8a25881653ce
Fayi FB <fayi@fayimora.com>
2022-06-29 20:52:04 +0100

    Formatting and sdkman

--- src/main/scala/com/fayimora/Main.scala
+++ src/main/scala/com/fayimora/Main.scala
@@ -4,9 +4,12 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse}
 
 object Main:
-  def handler(apiGatewayEvent: APIGatewayV2HTTPEvent, context: Context): APIGatewayV2HTTPResponse =
-    return APIGatewayV2HTTPResponse.builder()
+  def handler(
+      apiGatewayEvent: APIGatewayV2HTTPEvent,
+      context: Context
+  ): APIGatewayV2HTTPResponse =
+    return APIGatewayV2HTTPResponse
+      .builder()
       .withStatusCode(200)
       .withBody("Hello, world!")
       .build()
-
